5552e06b3dea10fdd46754cb4fdb103abe1008eb
zakattack9 <zakattack26111@gmail.com>
2019-09-29 22:03:47 -0700

    added overlay open animation

--- src/Overlay.js
+++ src/Overlay.js
@@ -5,28 +5,45 @@ import Icons from './SVGs/Icons';
 import './Overlay.css'
 
 class Overlay extends React.Component {
+  state = { overlayOpen: false };
 
-  render() {
-    return (
-      <div className="Overlay">
-        <div className="overlayBtn">
-          <div className="btnTop"></div>
-          <div className="btnBtm"></div>
-        </div>
+  overlayController() {
+    this.setState({ overlayOpen: !this.state.overlayOpen });
+  }
 
+  animateOverlay() {
+    const overlayNav = document.getElementById("overlayNav");
+    overlayNav.classList.toggle("overlayAppear");
+
+    const drawBackground = document.getElementById("overlayBackPath");
+    drawBackground.classList.toggle("drawBackground");
+
+    const navLink = [...document.getElementsByClassName("navLink")];
+    navLink.map(navLink => { navLink.classList.toggle("fadeInNavLink") });
+  }
+
+  componentDidUpdate() {
+    if (this.state.overlayOpen) {
+      this.animateOverlay();
+    }
+  }
+
+  renderNav() {
+    if (this.state.overlayOpen) {
+      return (
         <div id="overlayNav">
-          <OverlayBack className="overlayBack"></OverlayBack>
+          <OverlayBack id="overlayBack" pathId="overlayBackPath"></OverlayBack>
           <div className="navLinkWrapper">
-            <div className="navLink navHome">
+            <div className="navLink navHome" style={{ "animation-delay": "0.6s" }}>
               <Link to="/">Home</Link>
             </div>
-            <div className="navLink navProjects">
+            <div className="navLink navProjects" style={{ "animation-delay": "0.7s" }}>
               <Link to="/projects">Projects</Link>
             </div>
-            <div className="navLink navAbout">
+            <div className="navLink navAbout" style={{ "animation-delay": "0.8s" }}>
               <Link to="/about">About</Link>
             </div>
-            <div className="navLink navContact">
+            <div className="navLink navContact" style={{ "animation-delay": "0.9s" }}>
               <Link to="/contact">Contact</Link>
             </div>
           </div>
@@ -37,6 +54,18 @@ class Overlay extends React.Component {
             <Icons icon="linkedin" className="navIcon"></Icons>
           </div>
         </div>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div className="Overlay">
+        <div className="overlayBtn" onClick={() => this.overlayController()}>
+          <div className="btnTop"></div>
+          <div className="btnBtm"></div>
+        </div>
+        {this.renderNav()}
       </div>
     )
   }
---
+++ src/Projects/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Overlay from '../Overlay';
+
+class Projects extends React.Component {
+  render () {
+    return (
+      <Overlay></Overlay>
+    )
+  }
+}
+
+export default Projects;
