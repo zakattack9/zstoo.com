d1884146582f31449b65b579690043e408b2fe83
zakattack9 <zakattack26111@gmail.com>
2020-05-07 17:24:29 -0700

    refactored home css; fixed scrollmagic animations

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,67 +2,66 @@ import React from 'react';
 import { ScrollScene, addIndicators } from 'scrollscene';
 import { gsap } from 'gsap';
 import Zak from '../../SVGs/ZAK.svg';
-import { Home as Abstract } from '../../SVGs/SVG';
+import { Home as Abstract, Headline } from '../../SVGs/SVG';
 import Glow from '../../SVGs/HomeGlow.svg';
 import './Home.scss';
 
 class Home extends React.Component {
   componentDidMount () {
-    const abstractTimeline = gsap.timeline({ repeatDelay: 0.5 });
-    const abstractTween = gsap.from('.Home__path', {
+    const homeTimeline = gsap.timeline({ repeatDelay: 0.5 });
+    const abstractTween = gsap.to('.Home__path', {
       strokeDashoffset: 239,
+      opacity: 0,
       ease: 'power3.inOut',
-      // duration: 1.5,
+      // duration: 0.8,
       stagger: {
         each: 0.01,
-        from: 'center' // 'random' works great also
+        from: 'edges'
       }
     });
-    abstractTimeline.add(abstractTween);
+    homeTimeline.add(abstractTween);
 
-    console.log(this.props.duration);
-    const abstractTrigger = document.querySelector('#Home__trigger--abstract');
-    const abstract = document.querySelector('.Home__zak');
+    const headlineTween = gsap.to('.Home__headline', {
+      strokeDashoffset: 130,
+      ease: 'power3.inOut',
+      duration: 0.5,
+    });
+    homeTimeline.add(headlineTween, 0);
 
+    const abstractTrigger = document.querySelector('#Home__trigger');
     const scrollScene = new ScrollScene({
       triggerElement: abstractTrigger,
-      // toggle: {
-      //   element: abstract,
-      //   className: 'fadeOut',
-      //   reverse: true
-      // },
-      triggerHook: 0.5,
-      offset: 400,
-      // duration: this.props.duration + '%'
+      triggerHook: 0,
+      // offset: 150,
       gsap: {
-        timeline: abstractTimeline
+        timeline: homeTimeline
       },
-      duration: 400
+      duration: 900
     })
     scrollScene.Scene.setPin('.Home__wrapper');
     scrollScene.Scene.addIndicators({ name: 'home scene', colorEnd: '#FFFFFF' })
 
   }
 
-  // clean up ScrollScene with componentWillUnmount
+  componentWillUnmount () {
+    // clean up ScrollScene here
+  }
   
   render () {
     return (
       <div className="Home">
-        <div id="Home__trigger--abstract"></div>
-        <div className="Home__wrapper">
-          <div className="Home__art">
-            {/* <img className="Home__glow Home__glow--back" src={Glow} alt="abstract art glow"/> */}
-            <Abstract className="Home__abstract Home__abstract--back" />
-            <img className="Home__zak" src={Zak} alt="Zak"/>
-            <Abstract className="Home__abstract Home__abstract--front" />
-            {/* <img className="Home__glow Home__glow--front" src={Glow} alt="abstract art glow"/> */}
-          </div>
+        <div id="Home__trigger"></div>
+          <div className="Home__wrapper">
+            <div className="Home__art">
+              {/* <img className="Home__glow Home__glow--back" src={Glow} alt="abstract art glow"/> */}
+              <Abstract className="Home__abstract Home__abstract--back" />
+              <img className="Home__zak" src={Zak} alt="Zak"/>
+              <Abstract className="Home__abstract Home__abstract--front" />
+              {/* <img className="Home__glow Home__glow--front" src={Glow} alt="abstract art glow"/> */}
+            </div>
 
-          <div className="Home__headline">
-            I design and develop websites and applications.
+            <Headline className="Home__headline" />
           </div>
-        </div>
       </div>
     );
   }
