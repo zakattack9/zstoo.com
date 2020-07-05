6145a2d47ec67420164bb5b728027c6ec2e185ef
zakattack9 <zakattack26111@gmail.com>
2020-07-05 15:25:51 -0700

    Updates

---
+++ precache-manifest.b2dfea3ffd17d8e40a8f9b00f9e1f96b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8969af9382e169ec956a8adbb8289f72",
+    "url": "/zstoo.com/index.html"
+  },
+  {
+    "revision": "2c8cc3372653676558e9",
+    "url": "/zstoo.com/static/css/main.a8db8a57.chunk.css"
+  },
+  {
+    "revision": "7cf59cbf8f911ab919c5",
+    "url": "/zstoo.com/static/js/2.65cdf77e.chunk.js"
+  },
+  {
+    "revision": "e08b5b5b2e57557dcd4cda16dbb5f893",
+    "url": "/zstoo.com/static/js/2.65cdf77e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c8cc3372653676558e9",
+    "url": "/zstoo.com/static/js/main.83676b41.chunk.js"
+  },
+  {
+    "revision": "7dc7f47025beddf04cc7",
+    "url": "/zstoo.com/static/js/runtime-main.29fef2bd.js"
+  },
+  {
+    "revision": "bf120f7d17afd5feee4a6f2aa956d1a3",
+    "url": "/zstoo.com/static/media/AboutGlow.bf120f7d.png"
+  },
+  {
+    "revision": "100a064d3918bd26402a821d0a8a39c8",
+    "url": "/zstoo.com/static/media/HelveticaBoldOutline.100a064d.woff"
+  },
+  {
+    "revision": "00a27232161beb0c03f5297f546825b5",
+    "url": "/zstoo.com/static/media/HomeGlow.00a27232.png"
+  },
+  {
+    "revision": "aeb7ef1782a52520e42449898f408d46",
+    "url": "/zstoo.com/static/media/ObjectSansHeavy.aeb7ef17.woff"
+  },
+  {
+    "revision": "81ca083ad47dbf2a181aacd16f41a1cf",
+    "url": "/zstoo.com/static/media/ObjectSansHeavySlanted.81ca083a.woff"
+  },
+  {
+    "revision": "a9889097e462478f7852cf5f98de2572",
+    "url": "/zstoo.com/static/media/ObjectSansRegular.a9889097.woff"
+  },
+  {
+    "revision": "e86d10ee8d51abe2f93b169f0098d05f",
+    "url": "/zstoo.com/static/media/ObjectSansSlanted.e86d10ee.woff"
+  },
+  {
+    "revision": "c95e3e538c383ccab3a7af7712547e3f",
+    "url": "/zstoo.com/static/media/ProjectsGlow.c95e3e53.png"
+  },
+  {
+    "revision": "df800b5a29ef43d248304a7adba05a6b",
+    "url": "/zstoo.com/static/media/ZAK.df800b5a.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/zstoo.com/precache-manifest.b2dfea3ffd17d8e40a8f9b00f9e1f96b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zstoo.com/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
