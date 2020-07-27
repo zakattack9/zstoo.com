96efa8dd20a2ea5ce932503d942ed8d48b26d72d
zakattack9 <zakattack26111@gmail.com>
2020-07-26 22:33:04 -0700

    added basename for gh pages

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,7 +10,7 @@ import './App.scss';
 const App = () => {
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route path="/" exact>
             <Home />
