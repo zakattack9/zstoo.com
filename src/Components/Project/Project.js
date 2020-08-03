df74901704f7a65393aa73967f0b324601a609b1
zakattack9 <zakattack26111@gmail.com>
2020-08-03 12:01:51 -1000

    working on the project page

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -7,15 +7,14 @@ import PROJECT_DATA from '../../Data/ProjectData';
 import './Project.scss';
 
 const Project = props => {
-  const { projectId = 1 } = props;
+  const { projectId = 2 } = props;
   const project = PROJECT_DATA.find(project => project.id === projectId);
-
 
   return (
     <div className="Project">
       <div className="Project__info">
         {/* TOP SECTION */}
-        <NavLink className={"Project__NavLink Project__NavLink--AllProjects"} text={`All\nProjects`} lineWidth={110} href='/' left />
+        <NavLink className="Project__NavLink Project__NavLink--AllProjects" text={`All\nProjects`} lineWidth={110} href='/' left />
         <div className="Project__techStack">
           <div className="Project__techStackText Project__techStackText--title" style={{color: project.color}}>
             {`Tech\nStack`}
@@ -26,7 +25,7 @@ const Project = props => {
         </div>
         {/* MIDDLE SECTION */}
         <div className="Project__title">{project.name}</div>
-        <NavLink className={"Project__NavLink Project__NavLink--NextProject"} text={`Next\nProject`} lineWidth={15} />
+        <NavLink className="Project__NavLink Project__NavLink--NextProject" text={`Next\nProject`} lineWidth={15} />
         {/* GALLERY SECTION */}
         <Skim className="Project__Skim Project__Skim--gallery" type='toLeft' width={10} height={100} />
         <div className="Project__gallery">
@@ -37,7 +36,7 @@ const Project = props => {
         {/* BOTTOM SECTION */}
         <Skim className="Project__Skim Project__Skim--description" type='project' width={85} height={100} />
         <div className="Project__description">{project.fullDesc}</div>
-        <div className="Project__id">0{project.id}</div>
+        {/* <div className="Project__id">0{project.id}</div> */}
       </div>
 
       <div className="Project__art">
--- src/Data/ProjectData.js
+++ src/Data/ProjectData.js
@@ -4,7 +4,7 @@ const PROJECT_DATA = [
     id: 1,
     name: "Stockup",
     shortDesc: "Stock market news web scraper",
-    fullDesc: "Stockup is an application leveraging web scraping and APIs to gather stock news from various sources.\n\nStockup is an application leveraging web scraping and APIs to gather stock news from various sources.Stockup is an application leveraging web scraping and APIs to gather stock news from various sources.\n\nStockup is an application leveraging web scraping and APIs to gather stock news from various sources.Stockup is an application leveraging web scraping and APIs to gather stock news from various sources.\n\nStockup is an application leveraging web scraping and APIs to gather stock news from various sources.",
+    fullDesc: "Stockup is an application leveraging web scraping and APIs to gather news about stocks from various sources.\n\nThe UI is built with React and interacts with a Node.js backend that handles all the parsing of news articles and stock ticker information.",
     techStack: ['React', 'CSS', 'Node.js', 'Heroku'],
     githubLink: "",
     imageUrls: ['images/projects/Stockup1.png', 'images/projects/Stockup1.png'],
@@ -14,10 +14,11 @@ const PROJECT_DATA = [
     id: 2,
     name: "Lanakila Meals on Wheels",
     shortDesc: "Mass medium communication tool",
-    fullDesc: "",
-    techStack: [],
+    fullDesc: "Stockup is an application leveraging web scraping and APIs to gather stock news from various sources.\n\nStockup is an application leveraging web scraping and APIs to gather stock news from various sources.Stockup is an application leveraging web scraping and APIs to gather stock news from various sources.\n\nStockup is an application leveraging web scraping and APIs to gather stock news from various sources.Stockup is an application leveraging web scraping and APIs to gather stock news from various sources.\n\nStockup is an application leveraging web scraping and APIs to gather stock news from various sources.",
+    techStack: ['HTML', 'CSS', 'JS', 'AWS'],
     githubLink: "",
-    imageUrls: [] 
+    imageUrls: ['images/projects/Stockup1.png', 'images/projects/Stockup1.png'],
+    color: '#00BB95',
   },
   {
     id: 3,
