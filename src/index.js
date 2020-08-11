904163cae4fc13321f8a696e866b7f7225cddd3c
zakattack9 <zakattack26111@gmail.com>
2020-08-10 18:44:11 -1000

    reverting to non optimized build

--- src/index.js
+++ src/index.js
@@ -1,51 +1,45 @@
-import React, { Suspense, lazy } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 // import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Main from './Components/Main';
+import Project from './Components/Project/Project';
+import ContactForm from './Components/ContactForm/ContactForm';
 import Overlay from './Components/Overlay/Overlay';
 import Loader from './Components/Loader/Loader';
 import './index.scss';
-
-const Main = lazy(() => import('./Components/Main'));
-const Project = lazy(() => import('./Components/Project/Project'));
-const ContactForm = lazy(() => import('./Components/ContactForm/ContactForm'));
 
 const App = () => {
   return (
     <div className="App">
       <Router basename={process.env.PUBLIC_URL}>
-        <Suspense fallback={<Loader/>}>
-          <Overlay />
-          <Switch>
-            <Route path="/" exact>
-              <Main />
-            </Route>
-            <Route path="/home" exact>
-              <Main scrollTo="home" />
-            </Route>
-            <Route path="/projects" exact>
-              <Main scrollTo="projects" />
-            </Route>
-            <Route path="/about" exact>
-              <Main scrollTo="about" />
-            </Route>
-
-            <Route path="/project/:id">
-              <Project />
-            </Route>
-            <Route path="/project">
-              <Redirect to="/project/1" />
-            </Route>
-
-            <Route path="/contact">
-              <ContactForm />
-            </Route>
-
-            <Router path="/loader">
-              <Loader />
-            </Router>
-          </Switch>
-        </Suspense>
+        <Overlay />
+        <Switch>
+          <Route path="/" exact>
+            <Main />
+          </Route>
+          <Route path="/home" exact>
+            <Main scrollTo="home" />
+          </Route>
+          <Route path="/projects" exact>
+            <Main scrollTo="projects" />
+          </Route>
+          <Route path="/about" exact>
+            <Main scrollTo="about" />
+          </Route>
+          <Route path="/project/:id">
+            <Project />
+          </Route>
+          <Route path="/project">
+            <Redirect to="/project/1" />
+          </Route>
+          <Route path="/contact">
+            <ContactForm />
+          </Route>
+          <Router path="/loader">
+            <Loader />
+          </Router>
+        </Switch>
       </Router>
     </div>
   );
